171642a613d9eab3cd4ad44f5f17c54e2a1f7ff8
wtee <wtee@users.noreply.github.com>
2018-12-03 22:47:57 -0600

    Add ability to chose number of names. Add text and JSON output

--- random_names/routes.py
+++ random_names/routes.py
@@ -1,5 +1,7 @@
 def includeme(config):
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
-    config.add_route('american', '/american')
+    config.add_route('american', '/american.html')
+    config.add_route('american_text', '/american.txt')
+    config.add_route('american_json', '/american.json')
     config.add_route('american_data', '/american/data.json')
