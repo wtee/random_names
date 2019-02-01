5267c4a696f554f96400e022077458b9b486d6a1
wtee <wtee@users.noreply.github.com>
2019-01-31 22:23:51 -0600

    Add style and home page

--- random_names/views/default.py
+++ random_names/views/default.py
@@ -1,6 +1,6 @@
 from pyramid.view import view_config
 
 
-@view_config(route_name='home', renderer='../templates/mytemplate.jinja2')
+@view_config(route_name='home', renderer='../templates/home.jinja2')
 def my_view(request):
-    return {'project': 'random_names'}
+    return {'title': 'Random Names', 'description': 'A website for generating random names.'}
